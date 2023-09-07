48768b20160914df3523ff97efc121988b25d70b
Pablo SAZYCZ <PSAZYCZ@bazterrica.com.ar>
2023-09-07 13:19:59 -0300

    formulario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import "./App.css";
+import Formulario from "./components/Formulario/Formulario";
 
 import Header from "./components/Header/Header.js";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Formulario/>
     </div>
   );
 }
